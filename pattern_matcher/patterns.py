4286276a6db62346e9bdfd7dd7b75e7e7a771481
Damon Kelley <damon.kelley@gmail.com>
2015-06-30 01:07:47 -0500

    Add some comments and docstrings.

--- pattern_matcher/patterns.py
+++ pattern_matcher/patterns.py
@@ -1,10 +1,12 @@
 class Node(object):
+    """The object representation of a pattern node."""
     WILDCARD = '*'
 
     def __init__(self, value):
         self.value = value
 
     def is_wildcard(self):
+        """True if the Node's value matches the wildcard."""
         return self.value == self.WILDCARD
 
     def __str__(self):
@@ -15,20 +17,37 @@ class Node(object):
 
 
 class Pattern(object):
+    """The object representation of a pattern.
+
+    Most importantly, this objects calculates and stores the number of
+    wildcards contained in the pattern, and scores the wildcards based
+    on the position in the pattern.
+
+    When comparing Patterns, the first comparison is based on the number
+    of wildcards.  The Pattern with the fewer number of wildcards will always
+    win.  In the case of a tie, the score is used to break it.  A Pattern that
+    has more rightmost wildcards will always have a lower score.
+    """
     def __init__(self, nodes):
+        # Score of the pattern based on the index of it's wildcards.
         self.score = 0
         self.num_wildcards = 0
         self.nodes = [Node(node) for node in nodes]
         self.length = len(self.nodes)
-        self.__score_pattern()
+        self.__calculate()
 
-    def __score_pattern(self):
+    def __calculate(self):
+        """Calculate the number of wildcards score for the Pattern."""
         for index, node in enumerate(self.nodes):
             if node.is_wildcard():
                 self.num_wildcards += 1
-                self.score += self.length / float(index + 2)
+
+                # Scoring the pattern in this manner means that the pattern with
+                # the most rightmost wildcards will always have a lower score.
+                self.score += self.length / float(index + 1 + self.length)
 
     def has_wildcard(self):
+        """True if the Pattern has at least one wildcard."""
         return self.num_wildcards > 0
 
     def __str__(self):
@@ -43,27 +62,42 @@ class Patterns(object):
         self.min_wildcards = None
         self.min_score = None
         self.patterns = []
-        self.__add_patterns(patterns)
+        self.__configure(patterns)
 
-    def __add_patterns(self, patterns):
+    def __configure(self, patterns):
         for pattern in patterns:
+            # Create a new Pattern instance and add it to self.
             p = Pattern(pattern)
             self.patterns.append(p)
-            self._update_min_wildcards(p)
-            self._update_min_score(p)
 
-    def _update_min_wildcards(self, p):
-        if self.min_wildcards is None or p.num_wildcards < self.min_wildcards:
-            self.min_wildcards = p.num_wildcards
+            # If there is not yet a min_wildcard value, or if the current Pattern
+            # has a lower number of wildcards than the current min, then we
+            # want to set min_wildcards to the lower value.
+            if not self.has_min_wildcards() or p.num_wildcards < self.min_wildcards:
+                self.min_wildcards = p.num_wildcards
 
-    def _update_min_score(self, p):
-        if self.min_score is None or p.score < self.min_score:
-            self.min_score = p.score
+            # If there is not yet a min_score value, or if the current Pattern
+            # has a lower number score than the current min, then we
+            # want to set min_score to the lower value.
+            if not self.has_min_score() or p.score < self.min_score:
+                self.min_score = p.score
+
+    def has_min_score(self):
+        """True if min_score is not None."""
+        return self.min_score is not None
+
+    def has_min_wildcards(self):
+        """True if min_wilcards is not None."""
+        return self.min_wildcards is not None
 
     def get_best_patterns(self):
+        # Filter the patterns to get only the Pattern objects that have the
+        # least number of wildcards.
         patterns = [p for p in self.patterns if p.num_wildcards == self.min_wildcards]
 
-        if len(patterns) > 1:
+        # If multiple Pattern objects are returned, then we need to rate the
+        # matches by score and return those patterns.
+        if len(patterns) != 1:
             return [p for p in patterns if p.score == self.min_score]
 
-        return patterns.pop()
+        return patterns
