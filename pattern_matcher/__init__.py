eed4e5ea83fe275bc82489555b1a8a2760c9c728
Damon Kelley <damon.kelley@gmail.com>
2015-07-04 13:11:42 -0500

    Require Python 3 or greater.

--- pattern_matcher/__init__.py
+++ pattern_matcher/__init__.py
@@ -5,6 +5,9 @@ from .pattern_matcher import PathMatcher
 
 
 def main():
+    if sys.version_info.major < 3:
+        sys.exit('Python 3.x or greater is required.')
+
     patterns, _input = io.Input.parse(sys.stdin)
     output = io.Output(sys.stdout)
     pm = PathMatcher(patterns, _input, output)
